b2f234a0c6b78d8dee57d5454277a9149a5d0d82
DRBGnoles <96489980+DRBGnoles@users.noreply.github.com>
2023-10-26 15:53:31 -0400

    feat: create random password generator

---
+++ rbgpgen/rbgpgen.py
@@ -0,0 +1,33 @@
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+def generate_password():
+    password_length = int(input("Enter password length: "))
+
+    random.shuffle(characters)
+
+    password = []
+
+    for x in range(password_length):
+        password.append(random.choice(characters))
+
+    random.shuffle(password)
+
+
+    password = "".join(password)
+
+    print(password)
+
+
+option = input("Generate password? (y/n): ")
+
+if option == "y":
+    generate_password()
+elif option == "n":
+    print("Goodbye!")
+    quit()
+else:
+    print("Invalid option!")
+    quit()
